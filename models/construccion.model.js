e1e1231e85e7260af1dbd56a54d9848a6455d0a3
Juan Pablo <a01705408@tec.mx>
2024-03-13 12:37:49 -0600

    Se completó el lab 17

--- models/construccion.model.js
+++ models/construccion.model.js
@@ -8,12 +8,24 @@ module.exports = class Construccion {
         this.imagen = mi_imagen
     }
 
+
+    static fetchname(name) {
+        return db.execute('SELECT nombre FROM construccion WHERE nombre=?', 
+            [name]);
+    }
     //Este método servirá para guardar de manera persistente el nuevo objeto. 
     save() {
-        return db.execute(
-            'INSERT INTO construccion (nombre, imagen, username) VALUES (?, ?, "rommel49")',
-            [this.nombre, this.imagen]
-        );
+        return Construccion.fetchname(this.nombre)
+        .then(([rows, fieldData]) => {
+            // Verificar si hay alguna fila en el resultado
+            if (rows.length > 0) {
+                // Si ya existe una construcción con el mismo nombre, actualiza la imagen
+                return db.execute('UPDATE construccion SET imagen = ? WHERE nombre = ?', [this.imagen, this.nombre]);
+            } else {
+                // Si no existe una construcción con el mismo nombre, inserta una nueva
+                return db.execute('INSERT INTO construccion (nombre, imagen, username) VALUES (?, ?, "JuanPablo")', [this.nombre, this.imagen]);
+            }
+        });
     }
 
     //Este método servirá para devolver los objetos del almacenamiento persistente.
@@ -25,6 +37,8 @@ module.exports = class Construccion {
         return db.execute('SELECT * FROM construccion WHERE id=?', 
             [id]);
     }
+
+    
 
     static fetch(id) {
         if (id) {
